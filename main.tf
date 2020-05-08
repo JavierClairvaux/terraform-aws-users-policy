fc8e4c50a9b7f2deb99daa9d0bd602a7a52e5ca6
JavierClairvaux <javcabr@gmail.com>
2020-05-08 09:45:02 -0700

    added assume role block

--- main.tf
+++ main.tf
@@ -1,6 +1,9 @@
 provider "aws" {
-  region  = var.aws_region
+	region  = var.aws_region
 	profile = var.profile
+	assume_role {
+		role_arn		 = var.role
+	}
 }
 
 resource "random_string" "user_random" {
